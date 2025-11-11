b839302c3b509588a10ee4762a8ea2d476f1203c
Alasdair Wilson <alasdair.wlsn@googlemail.com>
2025-11-11 16:34:02 +0000

    fix epub build

--- docs/conf.py
+++ docs/conf.py
@@ -7,6 +7,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 
 project = "MetaBeeAI"
 release = "0.1.0"
+version = release
 year = datetime.now().year
 copyright = f"{year}, MetaBeeAI Contributors"
 
